100948693be9701fa240bc07a2c34c19c937ca6e
nataliealway <nlmaxwell@gmail.com>
2019-10-08 11:30:07 -0700

    Sucessfully added my chart to my html page, still need to get it to register the data

--- js/app.js
+++ js/app.js
@@ -121,3 +121,63 @@ new ProductImage('wine-glass', './imgs/wine-glass.jpg' );
 
 
 pickNewProduct();
+
+//Generate a sample ChartJS chart
+
+var genLabels = function(images) {
+  var labelsArr = [];
+  for (var i = 0; i < images.length; i++){
+    labelsArr.push(images[i].product);
+  }
+  console.log(labelsArr);
+  return labelsArr;
+};
+
+var genData = function(images) {
+  var dataArr = [];
+  for (var i = 0; i < images.length; i++) {
+    dataArr.push(images[i].clicks);
+  }
+  console.log(dataArr);
+  return dataArr
+}
+
+//myChart Function
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: genLabels(ProductImage.allImages),
+    datasets: [{
+      label: '# of Votes',
+      data: genData(ProductImage.allImages),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
