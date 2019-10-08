9344d13ae599a9613d4a7a24762b968c4de7faf6
nataliealway <nlmaxwell@gmail.com>
2019-10-07 20:20:55 -0700

    able to add function that records and display click data, but unable to get that data to show

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var leftImage = document.getElementById('left_img');
 var middleImage = document.getElementById('middle_img');
 var rightImage = document.getElementById('right_img');
 var totalClicks = 0;
+var totalRounds = 25;
 
 
 var rightImgOnPage = null;
@@ -18,49 +19,59 @@ var ProductImage = function(product, imgURL) {
   this.timeshown = 0;
   this.imgURL = imgURL;
 
-  ProductImage.allImages.push(this);
+  ProductImage.allProducts.push(this);
 
 }
 
-ProductImage.allImages = [];
+ProductImage.allProducts = [];
 
 //HELPER FUNCTIONS
 var renderNewImages = function(leftIndex, middleIndex, rightIndex) {
-  leftImage.src = ProductImage.allImages[leftIndex].imgURL;
-  middleImage.src = ProductImage.allImages[middleIndex].imgURL;
-  rightImage.src = ProductImage.allImages[rightIndex].imgURL;
+  leftImage.src = ProductImage.allProducts[leftIndex].imgURL;
+  middleImage.src = ProductImage.allProducts[middleIndex].imgURL;
+  rightImage.src = ProductImage.allProducts[rightIndex].imgURL;
 };
 
-var pickNewProduct = function(){
-  //PICK A NEW PRODUCT  
-  var leftIndex = Math.ceil(Math.random() * ProductImage.allImages.length -1);
-  var middleIndex = Math.ceil(Math.random() * ProductImage.allImages.length -1);
-  do {
-    var rightIndex = Math.ceil(Math.random () * ProductImage.allImages.length -1);
-  } while ( rightIndex === middleIndex === leftIndex);
+var randomProduct = function() {
+  return Math.ceil(Math.random() * ProductImage.allProducts.length -1);
+}
 
-  leftImgOnPage = ProductImage.allImages[leftIndex];
-  middleImgOnPage = ProductImage.allImages[middleIndex];
-  rightImgOnPage = ProductImage.allImages[rightIndex];
+var pickNewProduct = function(){ 
+  var leftIndex = randomProduct ();
+
+  do {
+    var middleIndex = randomProduct ();
+  } while (middleIndex === leftIndex);
+ 
+  do {
+    var rightIndex = randomProduct();
+  } while (rightIndex === leftIndex || rightIndex === middleIndex);
+
+  leftImgOnPage = ProductImage.allProducts[leftIndex];
+  middleImgOnPage = ProductImage.allProducts[middleIndex];
+  rightImgOnPage = ProductImage.allProducts[rightIndex];
+
+  renderNewImages(leftIndex, middleIndex, rightIndex);
 }
 
 
 //EVENT HANDLER 
 var handleClickOnImg = function(event){
-
-  if(totalClicks < 10 ) {
+console.log ('im working');
+  if(totalClicks < totalRounds ) {
     var imageClicked = event.target;
     var id = imageClicked.id;
 
     if(id === 'left_image' || id === 'middle_image'  || id === 'right_image'){
       if (id === 'left_image'){
          leftImgOnPage.clicks ++;
-      }
+      };
       if (id === 'middle_image') {
         middleImgOnPage.clicks ++;
+      };
       if(id === 'right_image'){
         rightImgOnPage.clicks ++;
-      }
+      };
 
       leftImgOnPage.timeshown ++;
       middleImgOnPage.timeshown ++;
@@ -68,39 +79,51 @@ var handleClickOnImg = function(event){
     
       pickNewProduct();
 
-      }
-    }
+      };
+    };
     totalClicks ++;
-    if(totalClicks === 10) {
+    if(totalClicks === totalRounds) {
       imageSectionTag.removeEventListener('click', handleClickOnImg)
       console.log('You have voted on 20 products, thanks!');
+      showFinalList();
     }
   };
 
+
+  var showFinalList = function (){
+    var resultsSection = document.getElementById('resultsSection');
+    var resultsList = document.createElement('ul');
+    resultsSection.appendChild(resultsList);
+    for (var i = 0; i < ProductImage.allProducts.length; i++){
+      var resultItem = document.createElement ('li');
+      resultItem.textContent = `${ProductImage.allProducts[i].name} was clicked ${ProductImage.allProducts[i].timesClicked} times, and shown ${ProductImage.allProducts[i].timesShown} times`;
+      resultsList.appendChild(resultItem);
+    }
+  }
   imageSectionTag.addEventListener('click', handleClickOnImg);
 
-}
 
-new ProductImage('Starwars Rollerboard', './img/bag.jpg')
-new ProductImage('Banana Cutter', './img/banana.jpg')
-new ProductImage('Bathroom', './img/bathroom')
-new ProductImage('boots', './img/boots.jpg' )
-new ProductImage('breakfast', './img/breakfast.jpg' )
-new ProductImage('bubblegym', './img/bubblegum.jpg' )
-new ProductImage('chair', './img/chair.jpg' )
-new ProductImage('cthulehe', './img/cthulhe.jpg' )
-new ProductImage('dog-duck', './img/dog-duck.jpg' )
-new ProductImage('dragon', './img/dragon.jpg' )
-new ProductImage('pen', './img/pen.jpg' )
-new ProductImage('petsweep', './img/pet-sweep.jpg' )
-new ProductImage( 'scissors', './img/scissors.jpg' )
-new ProductImage('shark', './img/shark.jpg' )
-new ProductImage('sweep', './img/sweep.png' )
-new ProductImage('tauntaun', './img/tauntaun.jpg' )
-new ProductImage('unicorn', './img/unicorn.jpg' )
-new ProductImage('usb', './img/usb.gif')
-new ProductImage('water-can', './img/water-can.jpg' )
-new ProductImage('wine-glass', './img/wine-glass.jpg' )
+
+new ProductImage('Starwars Rollerboard', './imgs/bag.jpg');
+new ProductImage('Banana Cutter', './imgs/banana.jpg');
+new ProductImage('Bathroom', './imgs/bathroom');
+new ProductImage('boots', './imgs/boots.jpg' );
+new ProductImage('breakfast', './imgs/breakfast.jpg' );
+new ProductImage('bubblegym', './imgs/bubblegum.jpg' );
+new ProductImage('chair', './imgs/chair.jpg' );
+new ProductImage('cthulehe', './imgs/cthulhe.jpg' );
+new ProductImage('dog-duck', './imgs/dog-duck.jpg' );
+new ProductImage('dragon', './imgs/dragon.jpg' );
+new ProductImage('pen', './imgs/pen.jpg' );
+new ProductImage('petsweep', './imgs/pet-sweep.jpg' );
+new ProductImage( 'scissors', './imgs/scissors.jpg' );
+new ProductImage('shark', './imgs/shark.jpg' );
+new ProductImage('sweep', './imgs/sweep.png' );
+new ProductImage('tauntaun', './imgs/tauntaun.jpg' );
+new ProductImage('unicorn', './imgs/unicorn.jpg' );
+new ProductImage('usb', './imgs/usb.gif');
+new ProductImage('water-can', './imgs/water-can.jpg' );
+new ProductImage('wine-glass', './imgs/wine-glass.jpg' );
 
 
 pickNewProduct();
