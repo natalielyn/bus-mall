476099df5cda564a1057ada18d5f19e44b80074f
nataliealway <nlmaxwell@gmail.com>
2019-10-08 09:16:00 -0700

    fixed results section to show data, still cant get images to change when clicking

--- js/app.js
+++ js/app.js
@@ -19,21 +19,21 @@ var ProductImage = function(product, imgURL) {
   this.timeshown = 0;
   this.imgURL = imgURL;
 
-  ProductImage.allProducts.push(this);
+  ProductImage.allImages.push(this);
 
 }
 
-ProductImage.allProducts = [];
+ProductImage.allImages = [];
 
 //HELPER FUNCTIONS
 var renderNewImages = function(leftIndex, middleIndex, rightIndex) {
-  leftImage.src = ProductImage.allProducts[leftIndex].imgURL;
-  middleImage.src = ProductImage.allProducts[middleIndex].imgURL;
-  rightImage.src = ProductImage.allProducts[rightIndex].imgURL;
+  leftImage.src = ProductImage.allImages[leftIndex].imgURL;
+  middleImage.src = ProductImage.allImages[middleIndex].imgURL;
+  rightImage.src = ProductImage.allImages[rightIndex].imgURL;
 };
 
 var randomProduct = function() {
-  return Math.ceil(Math.random() * ProductImage.allProducts.length -1);
+  return Math.ceil(Math.random() * ProductImage.allImages.length -1);
 }
 
 var pickNewProduct = function(){ 
@@ -47,9 +47,9 @@ var pickNewProduct = function(){
     var rightIndex = randomProduct();
   } while (rightIndex === leftIndex || rightIndex === middleIndex);
 
-  leftImgOnPage = ProductImage.allProducts[leftIndex];
-  middleImgOnPage = ProductImage.allProducts[middleIndex];
-  rightImgOnPage = ProductImage.allProducts[rightIndex];
+  leftImgOnPage = ProductImage.allImages[leftIndex];
+  middleImgOnPage = ProductImage.allImages[middleIndex];
+  rightImgOnPage = ProductImage.allImages[rightIndex];
 
   renderNewImages(leftIndex, middleIndex, rightIndex);
 };
@@ -95,16 +95,14 @@ console.log ('im working');
     var resultsSection = document.getElementById('resultsSection');
     var resultsList = document.createElement('ul');
     resultsSection.appendChild(resultsList);
-    for (var i = 0; i < ProductImage.allProducts.length; i++){
+    for (var i = 0; i < ProductImage.allImages.length; i++){
       var resultItem = document.createElement ('li');
-      resultItem.textContent = `${ProductImage.allProducts[i].name} was clicked ${ProductImage.allProducts[i].timesClicked} times, and shown ${ProductImage.allProducts[i].timesShown} times`;
+      resultItem.textContent = `${ProductImage.allImages[i].product} was clicked ${ProductImage.allImages[i].clicks} times, and shown ${ProductImage.allImages[i].timeshown} times`;
       resultsList.appendChild(resultItem);
     }
   };
 
   imageSectionTag.addEventListener('click', handleClickOnImg);
-
-
 
 new ProductImage('Starwars Rollerboard', './imgs/bag.jpg');
 new ProductImage('Banana Cutter', './imgs/banana.jpg');
